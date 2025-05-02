30a6f3893d3b43c5d17080505753d70ebac296b5
XxHAROLxX <diegocorzo76@gmail.com>
2025-05-02 16:45:07 -0500

    mensajes de error

--- Controllers/UsuarioController.php
+++ Controllers/UsuarioController.php
@@ -33,13 +33,11 @@ class UsuarioController{
             $_SESSION['usuario'] = $user['usuario'];
             header("Location: button.php"); // O a la página que quieras mostrar después de iniciar sesión
         } else {
-<<<<<<< HEAD
             header("Location: login.php?error=1"); // Redirigir a la página de inicio de sesión con un error
-=======
             session_start(); // Inicia la sesión si aún no está iniciada
             $_SESSION['login_error'] = "Usuario o contraseña incorrectos."; // Guarda el mensaje de error en la sesión
             header("Location: login.php"); // Redirige de vuelta a la página de inicio de sesión
->>>>>>> 5d93c01bdaf7b80c9b6b45a8bbcc42c1e85591ff
+
         }
     }
     
